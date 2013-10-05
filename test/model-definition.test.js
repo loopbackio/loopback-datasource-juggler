7f170a0542d5e000b5bc2691f79018546e6bf01b
Raymond Feng <raymond@strongloop.com>
2013-10-04 21:21:12 -0700

    Add more tests and fix toJSON

--- test/model-definition.test.js
+++ test/model-definition.test.js
@@ -14,15 +14,55 @@ describe('ModelDefinition class', function () {
         var modelBuilder = new ModelBuilder();
 
         var User = new ModelDefinition(modelBuilder, 'User', {
-            name: String,
+            name: "string",
             bio: ModelBuilder.Text,
             approved: Boolean,
             joinedAt: Date,
-            age: Number
+            age: "number"
         });
 
-        // console.log(User.toJSON());
+        User.build();
+        assert.equal(User.properties.name.type, String);
+        assert.equal(User.properties.bio.type, ModelBuilder.Text);
+        assert.equal(User.properties.approved.type, Boolean);
+        assert.equal(User.properties.joinedAt.type, Date);
+        assert.equal(User.properties.age.type, Number);
 
+
+        var json = User.toJSON();
+        assert.equal(json.name, "User");
+        assert.equal(json.properties.name.type, "String");
+        assert.equal(json.properties.bio.type, "Text");
+        assert.equal(json.properties.approved.type, "Boolean");
+        assert.equal(json.properties.joinedAt.type, "Date");
+        assert.equal(json.properties.age.type, "Number");
+
+        done();
+
+
+    });
+
+    it('should be able to define additional properties', function (done) {
+        var modelBuilder = new ModelBuilder();
+
+        var User = new ModelDefinition(modelBuilder, 'User', {
+            name: "string",
+            bio: ModelBuilder.Text,
+            approved: Boolean,
+            joinedAt: Date,
+            age: "number"
+        });
+
+        User.build();
+
+        User.defineProperty("id", {type: "number", id: true});
+        assert.equal(User.properties.name.type, String);
+        assert.equal(User.properties.bio.type, ModelBuilder.Text);
+        assert.equal(User.properties.approved.type, Boolean);
+        assert.equal(User.properties.joinedAt.type, Date);
+        assert.equal(User.properties.age.type, Number);
+
+        assert.equal(User.properties.id.type, Number);
         done();
 
 
@@ -46,10 +86,28 @@ describe('ModelDefinition class', function () {
             }
         });
 
-        // console.log(JSON.stringify(User.toJSON(), null, '  '));
+        User.build();
+        assert.equal(User.properties.name.type, String);
+        assert.equal(User.properties.bio.type, ModelBuilder.Text);
+        assert.equal(User.properties.approved.type, Boolean);
+        assert.equal(User.properties.joinedAt.type, Date);
+        assert.equal(User.properties.age.type, Number);
+        assert.equal(typeof User.properties.address.type, 'function');
+
+        var json = User.toJSON();
+        assert.equal(json.name, "User");
+        assert.equal(json.properties.name.type, "String");
+        assert.equal(json.properties.bio.type, "Text");
+        assert.equal(json.properties.approved.type, "Boolean");
+        assert.equal(json.properties.joinedAt.type, "Date");
+        assert.equal(json.properties.age.type, "Number");
+
+        assert.deepEqual(json.properties.address.type, { street: { type: 'String' },
+            city: { type: 'String' },
+            zipCode: { type: 'String' },
+            state: { type: 'String' } });
 
         done();
-
 
     });
 
@@ -73,11 +131,60 @@ describe('ModelDefinition class', function () {
 
         });
 
-        // console.log(JSON.stringify(User.toJSON(), null, '  '));
+        User.build();
+        assert.equal(User.properties.name.type, String);
+        assert.equal(User.properties.bio.type, ModelBuilder.Text);
+        assert.equal(User.properties.approved.type, Boolean);
+        assert.equal(User.properties.joinedAt.type, Date);
+        assert.equal(User.properties.age.type, Number);
+        assert.equal(User.properties.address.type, Address);
+
+
+        var json = User.toJSON();
+        assert.equal(json.name, "User");
+        assert.equal(json.properties.name.type, "String");
+        assert.equal(json.properties.bio.type, "Text");
+        assert.equal(json.properties.approved.type, "Boolean");
+        assert.equal(json.properties.joinedAt.type, "Date");
+        assert.equal(json.properties.age.type, "Number");
+
+        assert.equal(json.properties.address.type, 'Address');
 
         done();
 
-
     });
+
+    it('should report correct id names', function (done) {
+        var modelBuilder = new ModelBuilder();
+
+        var User = new ModelDefinition(modelBuilder, 'User', {
+            userId: {type: String, id: true},
+            name: "string",
+            bio: ModelBuilder.Text,
+            approved: Boolean,
+            joinedAt: Date,
+            age: "number"
+        });
+
+        assert.equal(User.idName(), 'userId');
+        assert.deepEqual(User.idNames(), ['userId']);
+        done();
+    });
+
+    it('should report correct table/column names', function (done) {
+        var modelBuilder = new ModelBuilder();
+
+        var User = new ModelDefinition(modelBuilder, 'User', {
+            userId: {type: String, id: true, oracle: {column: 'ID'}},
+            name: "string"
+        }, {oracle: {table: 'USER'}});
+
+        assert.equal(User.tableName('oracle'), 'USER');
+        assert.equal(User.tableName('mysql'), 'User');
+        assert.equal(User.columnName('oracle', 'userId'), 'ID');
+        assert.equal(User.columnName('mysql', 'userId'), 'userId');
+        done();
+    });
+
 });
 
