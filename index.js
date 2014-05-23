fc758abc2b7da2c31995ce773a1cbbc53126a0cc
Miroslav Bajtoš <miroslav@strongloop.com>
2014-05-23 11:03:24 +0200

    Replace connector base with loopback-connector
    Remove references to Connector and BaseSQL, connectors should require()
    loopback-connector instead of loopback-datasource-juggler.

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 exports.ModelBuilder = exports.LDL = require('./lib/model-builder.js').ModelBuilder;
 exports.DataSource = exports.Schema = require('./lib/datasource.js').DataSource;
 exports.ModelBaseClass = require('./lib/model.js');
-exports.Connector = require('./lib/connector.js');
 exports.ValidationError = require('./lib/validations.js').ValidationError;
-
-var baseSQL = './lib/sql';
-
-exports.__defineGetter__('BaseSQL', function () {
-    return require(baseSQL);
-});
-
 
 exports.__defineGetter__('version', function () {
     return require('./package.json').version;
