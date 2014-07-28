7ad22939abd4275ca76c8f082bedc3d6ae8ec348
Raymond Feng <raymond@strongloop.com>
2014-07-28 09:00:05 -0700

    Export GeoPoint class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.ModelBuilder = exports.LDL = require('./lib/model-builder.js').ModelBuilder;
 exports.DataSource = exports.Schema = require('./lib/datasource.js').DataSource;
 exports.ModelBaseClass = require('./lib/model.js');
+exports.GeoPoint = require('./lib/geo.js').GeoPoint;
 exports.ValidationError = require('./lib/validations.js').ValidationError;
 
 exports.__defineGetter__('version', function () {
