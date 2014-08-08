f671c9c726761a246fcafc058e940db26b617c13
Raymond Feng <raymond@strongloop.com>
2014-08-08 01:20:57 -0700

    Clean up the mixin processing

--- test/fixtures/mixins/demo.js
+++
@@ -1,5 +0,0 @@
-module.exports = function timestamps(Model, options) {
-  
-  Model.demoMixin = options.ok;
-  
-};
--- test/mixins.test.js
+++ test/mixins.test.js
@@ -1,14 +1,44 @@
 // This test written in mocha+should.js
 var should = require('./init.js');
-var assert = require('assert');
-var path = require('path');
 
 var jdb = require('../');
 var ModelBuilder = jdb.ModelBuilder;
 var DataSource = jdb.DataSource;
 var Memory = require('../lib/connectors/memory');
 
-var mixins = jdb.mixins;
+var modelBuilder = new ModelBuilder();
+var mixins = modelBuilder.mixins;
+
+function timestamps(Model, options) {
+
+  Model.defineProperty('createdAt', { type: Date });
+  Model.defineProperty('updatedAt', { type: Date });
+
+  var originalBeforeSave = Model.beforeSave;
+  Model.beforeSave = function(next, data) {
+    Model.applyTimestamps(data, this.isNewRecord());
+    if (data.createdAt) {
+      this.createdAt = data.createdAt;
+    }
+    if (data.updatedAt) {
+      this.updatedAt = data.updatedAt;
+    }
+    if (originalBeforeSave) {
+      originalBeforeSave.apply(this, arguments);
+    } else {
+      next();
+    }
+  };
+
+  Model.applyTimestamps = function(data, creation) {
+    data.updatedAt = new Date();
+    if (creation) {
+      data.createdAt = data.updatedAt;
+    }
+  };
+}
+
+mixins.define('TimeStamp', timestamps);
 
 describe('Model class', function () {
   
@@ -20,56 +50,35 @@ describe('Model class', function () {
     });
   });
   
-  it('should load mixins from directory', function() {
-    var expected = [ 'TimeStamp', 'Example', 'Address', 'Demo', 'Other' ];
-    mixins.load(path.join(__dirname, 'fixtures', 'mixins'));
-    mixins.registry.should.have.property('TimeStamp');
-    mixins.registry.should.have.property('Example');
-    mixins.registry.should.have.property('Address');
-    mixins.registry.should.have.property('Demo');
-    mixins.registry.should.have.property('Other');
-  });
-  
   it('should apply a mixin class', function() {
-    var memory = new DataSource({connector: Memory});
-    var Item = memory.createModel('Item', { name: 'string' }, {
-      mixins: { TimeStamp: true, demo: true, Address: true }
-    });
-    
-    var modelBuilder = new ModelBuilder();
     var Address = modelBuilder.define('Address', {
       street: { type: 'string', required: true },
       city: { type: 'string', required: true }
     });
+
+    var memory = new DataSource('mem', {connector: Memory}, modelBuilder);
+    var Item = memory.createModel('Item', { name: 'string' }, {
+      mixins: { TimeStamp: true, demo: true, Address: true }
+    });
+
+    var properties = Item.definition.properties;
     
-    Item.mixin(Address);
-    
-    var def = memory.getModelDefinition('Item');
-    var properties = def.toJSON().properties;
-    
-    // properties.street.should.eql({ type: 'String', required: true });
-    // properties.city.should.eql({ type: 'String', required: true });
+    properties.street.should.eql({ type: String, required: true });
+    properties.city.should.eql({ type: String, required: true });
   });
   
   it('should apply mixins', function(done) {
-    var memory = new DataSource({connector: Memory});
+    var memory = new DataSource('mem', {connector: Memory}, modelBuilder);
     var Item = memory.createModel('Item', { name: 'string' }, {
-      mixins: { TimeStamp: true, demo: { ok: true }, Address: true }
+      mixins: { TimeStamp: true, demo: { ok: true } }
     });
     
     Item.mixin('Example', { foo: 'bar' });
     Item.mixin('other');
     
-    var def = memory.getModelDefinition('Item');
-    var properties = def.toJSON().properties;
-    properties.createdAt.should.eql({ type: 'Date' });
-    properties.updatedAt.should.eql({ type: 'Date' });
-    
-    // properties.street.should.eql({ type: 'String', required: true });
-    // properties.city.should.eql({ type: 'String', required: true });
-    
-    Item.demoMixin.should.be.true;
-    Item.prototype.otherMixin.should.be.true;
+    var properties = Item.definition.properties;
+    properties.createdAt.should.eql({ type: Date });
+    properties.updatedAt.should.eql({ type: Date });
     
     Item.create({ name: 'Item 1' }, function(err, inst) {
       inst.createdAt.should.be.a.date;
