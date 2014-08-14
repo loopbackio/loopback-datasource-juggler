f2025c3995f542b8679e0261346e6271f667a4ef
Fabien Franzen <info@atelierfabien.be>
2014-08-14 09:40:03 +0200

    Allow partial list of ids for sortByIds

--- test/basic-querying.test.js
+++ test/basic-querying.test.js
@@ -87,6 +87,33 @@ describe('basic-querying', function () {
         done();
       });
     });
+    
+    it('should sortByIds', function(done) {
+      User.find(function(err, users) {
+        sorted = User.sortByIds([6, 5, 4, 3, 2, 1], users);
+        var names = sorted.map(function(u) { return u.name; });
+        names.should.eql(['f', 'e', 'd', 'c', 'b', 'a']);
+        done();
+      });
+    });
+    
+    it('should sortByIds - partial ids (1)', function(done) {
+      User.find(function(err, users) {
+        sorted = User.sortByIds([6, 5, 4], users);
+        var names = sorted.map(function(u) { return u.name; });
+        names.should.eql(['f', 'e', 'd', 'a', 'b', 'c']);
+        done();
+      });
+    });
+    
+    it('should sortByIds - partial ids (2)', function(done) {
+      User.find(function(err, users) {
+        sorted = User.sortByIds([5, 3, 2], users);
+        var names = sorted.map(function(u) { return u.name; });
+        names.should.eql(['e', 'c', 'b', 'a', 'd', 'f']);
+        done();
+      });
+    });
 
   });
 
