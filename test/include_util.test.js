0864bf7154aa42298400de9b26bdfb294a162403
Wert_Lex <a.v.povar@gmail.com>
2015-10-10 19:21:06 +0500

    with updated map which stores original key and tests for them

--- test/include_util.test.js
+++ test/include_util.test.js
@@ -32,6 +32,54 @@ describe('include_util', function(){
     });
   });
 
+  describe('#buildOneToOneIdentityMapWithOrigKeys', function(){
+    it('should return an object with keys', function(){
+      var objs = [
+        {id: 11, letter: "A"},
+        {id: 22, letter: "B"}
+      ];
+      var result = includeUtils.buildOneToOneIdentityMapWithOrigKeys(objs, "id");
+      result.should.be.an.instanceOf(Object);
+      result.should.have.property("11");
+      result.should.have.property("22");
+      Object.keys(result).should.have.lengthOf(2);  // no additional properties
+    });
+    it('should return all stringized keys with .keys method', function(){
+      var objs = [
+        {id: 11, letter: "A"},
+        {id: 22, letter: "B"},
+        {id: "cc", letter: "C"}
+      ];
+      var result = includeUtils.buildOneToOneIdentityMapWithOrigKeys(objs, "id");
+      var keys = result.keys;
+      keys.should.be.instanceOf(Array);
+      keys.should.have.lengthOf(3);
+      keys.should.be.eql(['11', '22', 'cc']);
+    });
+    it("should return all original keys with .originalKeys method", function(){
+      var objs = [
+        {id: 11, letter: "A"},
+        {id: 22, letter: "B"},
+        {id: "vv", letter: "V"}
+      ];
+      var result = includeUtils.buildOneToOneIdentityMapWithOrigKeys(objs, "id");
+      var origKeys = result.originalKeys;
+      origKeys.should.be.instanceOf(Array);
+      origKeys.should.have.lengthOf(3);
+      origKeys.should.be.eql([11, 22, 'vv']);
+    });
+    it('should have .keys and .originalKeys in same order', function(){
+      var objs = [
+        {id: 11, letter: "A"},
+        {id: 22, letter: "B"},
+        {id: "vv", letter: "V"}
+      ];
+      var result = includeUtils.buildOneToOneIdentityMapWithOrigKeys(objs, "id");
+      var keys = result.keys;
+      var origKeys = result.originalKeys;
+      origKeys.map(function(a){return a.toString();}).should.be.eql(keys);
+    });
+  });
     describe('#buildOneToManyIdentityMap', function(){
         it('should return an object with keys', function(){
             var objs = [
