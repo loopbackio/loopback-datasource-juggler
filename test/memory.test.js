loopbackio/loopback-datasource-juggler

commit 632898b02272a010076d65ea440d88f949bc09bc

Add support for matching array values à la mongo.

lib/connectors/memory.js
@@ -416,7 +416,20 @@ function applyFilter(filter) {
         }
       }
 
-      if (test(where[key], getValue(obj, key))) {
+      var value = getValue(obj, key);
+      // Support referencesMany and other embedded relations
+      // Also support array types. Mongo, possibly PostgreSQL
+      if (Array.isArray(value)) {
+        var matcher  = where[key];
+        return value.some(function (v, i) {
+          var filter = {where: {}};
+          filter.where[i] = matcher;
+          return applyFilter(filter)(value);
+        });
+      }
+
+
+      if (test(where[key], value)) {
         return true;
       }
 
@@ -428,7 +441,7 @@ function applyFilter(filter) {
         var subFilter = {where: {}};
         var subKey = key.substring(dotIndex+1);
         subFilter.where[subKey] = where[key];
-        return !!subValue.filter(applyFilter(subFilter)).length
+        return subValue.some(applyFilter(subFilter));
       }
 
       return false;

test/memory.test.js
@@ -134,7 +134,6 @@ describe('Memory connector', function() {
     });
   });
 
-  // describe.only('Query for memory connector', function() {
   describe('Query for memory connector', function() {
     var ds = new DataSource({
       connector: 'memory'
@@ -306,6 +305,22 @@ describe('Memory connector', function() {
       User.find({where: {birthday: {between: [new Date(1990,0), Date.now()]}}},
                 function(err, users) {
         should(users.length).be.equal(0);
+        done();
+      });
+    });
+
+    it('should successfully extract 2 users matching array values', function (done) {
+      User.find({
+        where: {
+          'children': {
+            regexp: /an/
+          }
+        }
+      }, function (err, users) {
+        should.not.exist(err);
+        users.length.should.be.equal(2);
+        users[0].name.should.be.equal('John Lennon');
+        users[1].name.should.be.equal('George Harrison');
         done();
       });
     });
@@ -492,7 +507,8 @@ describe('Memory connector', function() {
             { name: 'Paul McCartney' },
             { name: 'George Harrison' },
             { name: 'Ringo Starr' },
-          ]
+          ],
+          children: ['Sean', 'Julian']
         },
         {
           seq: 1,
@@ -512,9 +528,10 @@ describe('Memory connector', function() {
             { name: 'John Lennon' },
             { name: 'George Harrison' },
             { name: 'Ringo Starr' },
-          ]
+          ],
+          children: ['Stella', 'Mary', 'Heather', 'Beatrice', 'James']
         },
-        {seq: 2, name: 'George Harrison', order: 5, vip: false},
+        {seq: 2, name: 'George Harrison', order: 5, vip: false, children: ['Dhani']},
         {seq: 3, name: 'Ringo Starr', order: 6, vip: false},
         {seq: 4, name: 'Pete Best', order: 4},
         {seq: 5, name: 'Stuart Sutcliffe', order: 3, vip: true}

commit d8678a1f03ee0796a91c97e87b8f9487c511a91e

Correction of a regression introduced by commit 632898b: when querying an empty array ([]) with a 'neq' filter, there were no matching.

lib/connectors/memory.js
@@ -421,6 +421,11 @@ function applyFilter(filter) {
       // Also support array types. Mongo, possibly PostgreSQL
       if (Array.isArray(value)) {
         var matcher  = where[key];
+        // The following condition is for the case where we are querying with
+        // a neq filter, and when the value is an empty array ([]).
+        if (matcher.neq !== undefined && value.length <= 0) {
+          return true;
+        }
         return value.some(function (v, i) {
           var filter = {where: {}};
           filter.where[i] = matcher;

test/memory.test.js
@@ -338,6 +338,19 @@ describe('Memory connector', function() {
       });
     });
 
+    it('should successfully extract 5 users matching a neq filter over array values', function (done) {
+      User.find({
+        where: {
+          'children': {neq: 'Dhani'}
+        }
+      }, function (err, users) {
+        should.not.exist(err);
+        console.log(users);
+        users.length.should.be.equal(5);
+        done();
+      });
+    });
+
     it('should count using date string', function(done) {
       User.count({birthday: {lt: new Date(1990,0).toISOString()}},
         function(err, count) {
@@ -546,7 +559,7 @@ describe('Memory connector', function() {
         },
         {seq: 2, name: 'George Harrison', order: 5, vip: false, children: ['Dhani']},
         {seq: 3, name: 'Ringo Starr', order: 6, vip: false},
-        {seq: 4, name: 'Pete Best', order: 4},
+        {seq: 4, name: 'Pete Best', order: 4, children: []},
         {seq: 5, name: 'Stuart Sutcliffe', order: 3, vip: true}
       ];
 