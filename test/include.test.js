017a4016ceac68bafcefc121473c0663efb87b80
Tom Kirkpatrick <tom@systemseed.com>
2016-01-08 11:09:29 +0100

    Add unit test to verify fix for #754

--- test/include.test.js
+++ test/include.test.js
@@ -427,6 +427,30 @@ describe('include', function () {
     });
   });
 
+  // https://github.com/strongloop/loopback-datasource-juggler/issues/754.
+  it('should save related items separately', function(done) {
+    var self = this;
+    User.find({
+      include: 'posts'
+    })
+      .then(function(users) {
+        var posts = users[0].posts();
+        posts.should.have.length(3);
+        return users[0].save();
+      })
+      .then(function(updatedUser) {
+        return User.findById(updatedUser.id, {
+          include: 'posts'
+        });
+      })
+      .then(function(user) {
+        var posts = user.posts();
+        posts.should.have.length(3);
+      })
+      .then(done)
+      .catch(done);
+  });
+
   describe('performance', function () {
     var all;
     beforeEach(function() {
