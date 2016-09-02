95bc2e93cc7ff1f9460fe2228317cba8de6084c7
Simon Ho <simonho@ca.ibm.com>
2016-09-01 21:06:08 -0700

    Remove expired item before executing expire
    The expire feature is falsely returning 204 instead of 404 because it is
    not removing expired items before execution.

--- test/kvao/expire.suite.js
+++ test/kvao/expire.suite.js
@@ -35,6 +35,18 @@ module.exports = function(dataSourceFactory, connectorCapabilities) {
         .then(function(value) { should.equal(value, null); });
     });
 
+    it('returns error when expiring a key that has expired', function() {
+      return Promise.resolve(CacheItem.set('expired-key', 'a-value', 1))
+        .delay(20)
+        .then(function() { return CacheItem.expire('expired-key', 1000); })
+        .then(
+          function() { throw new Error('expire() should have failed'); },
+          function(err) {
+            err.message.should.match(/expired-key/);
+            err.should.have.property('statusCode', 404);
+          });
+    });
+
     it('returns error when key does not exist', function() {
       return CacheItem.expire('key-does-not-exist', 1).then(
         function() { throw new Error('expire() should have failed'); },
