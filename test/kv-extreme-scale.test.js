ddaeb233f290927c5f95fd863987b1215d49a771
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2016-10-06 14:03:41 +0200

    spike: kv-extreme-scale connector
    DO NOT MERGE!

---
+++ test/kv-extreme-scale.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// temporary config not comitted to git,
+// this will need rework for the real test suite
+var dbConfig = require('./kv-extreme-scale.config');
+
+var DataSource = require('..').DataSource;
+var connector = require('../lib/connectors/kv-extreme-scale');
+var extend = require('util')._extend;
+
+function createDataSource() {
+  var settings = extend(dbConfig, {connector: connector});
+  return new DataSource(settings);
+};
+
+describe('ExtremeScale connector', function() {
+  this.timeout(10000);
+  describe('Juggler API', function() {
+    require('./kvao.suite')(createDataSource);
+  });
+});
