2320df12273f78296d62663bce29b5e86a000d8d
Simon Ho <simonho@ca.ibm.com>
2017-01-05 19:32:41 -0800

    Refactor flush to deleteAll
    - Rename `flush` to `deleteAll`
    - Add `delete`
    - Detect `delete/deleteAll` before running downstream test suites
    - Fall back to unoptimized `deleteAll` when connector does not support
      `deleteAll` but supports `delete`
    - Return 501 for connectors not supporting `delete` or `deleteAll`

---
+++ test/kvao/delete-all.suite.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bdd = require('../helpers/bdd-if');
+const helpers = require('./_helpers');
+const should = require('should');
+
+module.exports = function(dataSourceFactory, connectorCapabilities) {
+  var supportsDeleteAll = 'deleteAll' in dataSourceFactory().connector;
+
+  bdd.describeIf(supportsDeleteAll, 'deleteAll', function() {
+    let CacheItem;
+    beforeEach(function unpackContext() {
+      CacheItem = helpers.givenCacheItem(dataSourceFactory);
+    });
+
+    it('removes all key-value pairs for the given model', function() {
+      return helpers.givenKeys(CacheItem, ['key1', 'key2'])
+        .then(() => CacheItem.deleteAll())
+        .then(() => CacheItem.keys())
+        .then((keys) => {
+          should(keys).eql([]);
+        });
+    });
+
+    it('does not remove data from other existing models', function() {
+      var AnotherModel = dataSourceFactory().createModel('AnotherModel');
+      return helpers.givenKeys(CacheItem, ['key1', 'key2'])
+        .then(() => helpers.givenKeys(AnotherModel, ['key3', 'key4']))
+        .then(() => CacheItem.deleteAll())
+        .then(() => AnotherModel.keys())
+        .then((keys) => {
+          should(keys).eql(['key3', 'key4']);
+        });
+    });
+  });
+};
