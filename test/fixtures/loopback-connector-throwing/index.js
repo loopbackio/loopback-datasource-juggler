af1d9596130c554a71c0bba627af2972bd3a9d66
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-01-31 10:31:54 +0100

    Fix datasource to report connector-loading errors
    Before this change, when resolving full connector path, all errors were
    ignored. As a result, when the connector was installed but not
    correctly built (e.g. loopback-connector-db2 which uses a native addon),
    a very confusing message was reported by LoopBack.
    
    In this commit, I am fixing the code handling `require()` errors
    to ignore only MODULE_NOT_FOUND errors that contain the name
    of the required module.

---
+++ test/fixtures/loopback-connector-throwing/index.js
@@ -0,0 +1,8 @@
+// Copyright IBM Corp. 2013,2016. All Rights Reserved.
+// Node module: loopback-datasource-juggler
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+throw new Error('expected test error');
