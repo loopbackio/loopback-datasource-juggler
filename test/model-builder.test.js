b0b377af0ca48de39ff174d271a09df5ff1e89ba
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-01-08 09:25:31 +0100

    Set model constructor name to model name
    Rework the code building model constructors to leverage `Function` class
    and dynamically emit a constructor function named after the model.
    
    Before this change, all model classes were called "ModelConstructor",
    which made debugging difficult.
    
    After this change, a model class for model "User" is called "User.
    
    Because not all valid model names are also valid JavaScript identifiers,
    we implement a simple sanitization technique (replacing characters like
    "-", "." and ":" with underscore "_") and fall back to legacy
    "ModelConstructor" if the model name is still not a valid JS identifier.

---
+++ test/model-builder.test.js
@@ -0,0 +1,56 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: loopback-datasource-juggler
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const should = require('./init.js');
+
+const juggler = require('../');
+var ModelBuilder = juggler.ModelBuilder;
+
+describe('ModelBuilder', () => {
+  describe('define()', () => {
+    let builder;
+
+    beforeEach(givenModelBuilderInstance);
+
+    it('sets correct "modelName" property', () => {
+      const MyModel = builder.define('MyModel');
+      MyModel.should.have.property('modelName', 'MyModel');
+    });
+
+    it('sets correct "name" property on model constructor', () => {
+      const MyModel = builder.define('MyModel');
+      MyModel.should.have.property('name', 'MyModel');
+    });
+
+    describe('model class name sanitization', () => {
+      it('converts "-" to "_"', () => {
+        const MyModel = builder.define('Grand-child');
+        MyModel.should.have.property('name', 'Grand_child');
+      });
+
+      it('converts "." to "_"', () => {
+        const MyModel = builder.define('Grand.child');
+        MyModel.should.have.property('name', 'Grand_child');
+      });
+
+      it('converts ":" to "_"', () => {
+        const MyModel = builder.define('local:User');
+        MyModel.should.have.property('name', 'local_User');
+      });
+
+      it('falls back to legacy "ModelConstructor" in other cases', () => {
+        const MyModel = builder.define('Grand\tchild');
+        MyModel.should.have.property('name', 'ModelConstructor');
+      });
+    });
+
+    function givenModelBuilderInstance() {
+      builder = new ModelBuilder();
+    }
+  });
+});
+
