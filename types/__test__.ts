c7b88e9b8e8d225d0ae6a153f223b820195ab814
Miroslav Bajtoš <mbajtoss@gmail.com>
2020-02-28 10:07:26 +0100

    Add ObserverMixin members to ModelBase typings
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

---
+++ types/__test__.ts
@@ -0,0 +1,90 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: loopback-datasource-juggler
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+// A test file to verify types described by our .d.ts files.
+// The code in this file is only compiled, we don't run it via Mocha.
+
+import {
+  DataSource,
+  KeyValueModel,
+  ModelBase,
+  ModelBaseClass,
+  PersistedModel,
+  PersistedModelClass,
+} from '..';
+
+const db = new DataSource('db', {connector: 'memory'});
+
+//-------
+// ModelBase should provide ObserverMixin APIs as static methods
+//-------
+//
+(function() {
+  const Data = db.createModel('Data');
+
+  // An operation hook can be installed
+  Data.observe('before save', async ctx => {});
+
+  // Context is typed and provides `Model` property
+  Data.observe('before save', async ctx => {
+    console.log(ctx.Model.modelName);
+  });
+
+  // ModelBaseClass can be assigned to `typeof ModelBase`
+  // Please note that both `ModelBaseClass` and typeof ModelBase`
+  // are different ways how to describe a class constructor of a model.
+  // In this test we are verifying that the value returned by `createModel`
+  // can be assigned to both types.
+  const modelTypeof: typeof ModelBase = Data;
+  const modelCls: ModelBaseClass = modelTypeof;
+});
+
+//-------
+// PersistedModel should provide ObserverMixin APIs as static methods
+//-------
+(function () {
+  const Product = db.createModel<PersistedModelClass>(
+    'Product',
+    {name: String},
+    {strict: true}
+  );
+
+  // It accepts async function
+  Product.observe('before save', async ctx => {});
+
+  // It accepts callback-based function
+  Product.observe('before save', (ctx, next) => {
+    next(new Error('test error'));
+  });
+
+  // ctx.Model is a PersistedModel class constructor
+  Product.observe('before save', async ctx => {
+    await ctx.Model.findOne();
+  });
+
+  // PersistedModelClass can be assigned to `typeof PersistedModel`
+  // Please note that both `PersistedModelClass` and typeof PersistedModel`
+  // are different ways how to describe a class constructor of a model.
+  // In this test we are verifying that the value returned by `createModel`
+  // can be assigned to both types.
+  const modelTypeof: typeof PersistedModel = Product;
+  const modelCls: PersistedModelClass = modelTypeof;
+});
+
+//-------
+// KeyValueModel should provide ObserverMixin APIs as static methods
+//-------
+(function () {
+  const kvdb = new DataSource({connector: 'kv-memory'});
+  const CacheItem = kvdb.createModel<typeof KeyValueModel>('CacheItem');
+
+  // An operation hook can be installed
+  CacheItem.observe('before save', async ctx => {});
+
+  // ctx.Model is a KeyValueModel class constructor
+  CacheItem.observe('before save', async ctx => {
+    await ctx.Model.expire('key', 100);
+  });
+});
