5f41eabf279013328443fcc20390e05ce310c5d8
Dimitris <dimitris_ha@hotmail.com>
2020-03-12 15:53:37 +0200

    prefer single findOne result in tests

--- test/basic-querying.test.js
+++ test/basic-querying.test.js
@@ -961,17 +961,17 @@ describe('basic-querying', function() {
 
     describe('check __parent relationship in embedded models', () => {
       it('should fill the parent in embedded model', async () => {
-        const [user] = await User.find({where: {name: 'John Lennon'}});
+        const user = await User.findOne({where: {name: 'John Lennon'}});
         user.should.have.property('address').which.has.property('__parent').which.is
           .instanceof(User).and.equals(user);
       });
       it('should assign the container model as parent in list property', async () => {
-        const [user] = await User.find({where: {name: 'John Lennon'}});
+        const user = await User.findOne({where: {name: 'John Lennon'}});
         user.should.have.property('friends').which.has.property('parent').which.is
           .instanceof(User).and.equals(user);
       });
       it('should have the complete chain of parents available in embedded list element', async () => {
-        const [user] = await User.find({where: {name: 'John Lennon'}});
+        const user = await User.findOne({where: {name: 'John Lennon'}});
         user.friends.forEach((userFriend) => {
           userFriend.should.have.property('__parent').which.equals(user);
         });
