42f9c366bd96f40cde29affeb272499f9cd90ec4
Samuel Hughes <shughes1@uk.ibm.com>
2025-02-19 10:59:52 +0000

    fix: stop using _extends
    Signed-off-by: Samuel Hughes <shughes1@uk.ibm.com>

--- test/kvao.suite.js
+++ test/kvao.suite.js
@@ -6,12 +6,11 @@
 'use strict';
 
 const debug = require('debug')('test');
-const extend = require('util')._extend;
 const fs = require('fs');
 const path = require('path');
 
 module.exports = function(dataSourceFactory, connectorCapabilities) {
-  connectorCapabilities = extend({
+  connectorCapabilities = Object.assign({
     // Even when the backend supports millisecond precision,
     // it's better to use intervals at least 10ms long in the tests
     ttlPrecision: 10,
